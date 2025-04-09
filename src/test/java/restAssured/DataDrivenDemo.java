130167a3c1cfd0c8044e8b999f3d7cf779dfe781
stephin-abraham <stephinphilip270@gmail.com>
2025-04-09 12:54:12 +0530

    adding the DataDriven codes to the LOCAL API

--- src/test/java/restAssured/DataDrivenDemo.java
+++ src/test/java/restAssured/DataDrivenDemo.java
@@ -2,14 +2,13 @@ package restAssured;
 
 import io.restassured.http.ContentType;
 import org.json.JSONObject;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 
 public class DataDrivenDemo extends DataForTest{
 
-    @Test(dataProvider = "Data_for_Post")
+    @Test(dataProvider = "Data_for_Post", priority = 0)
     public void postTest(String name, int age){
         baseURI = "http://localhost:3000/";
         JSONObject req = new JSONObject();
@@ -25,6 +24,16 @@ public class DataDrivenDemo extends DataForTest{
 
                 .then()
                 .statusCode(201)
+                .log().all();
+    }
+    @Test(dataProvider = "Data_for_Delete",priority = 1)
+    public void deletePost(String userId){
+        baseURI = "http://localhost:3000/";
+        given()
+                .when()
+                .delete("sample/"+userId)
+                .then()
+                .statusCode(200)
                 .log().all();
     }
 }
--- src/test/java/restAssured/DataForTest.java
+++ src/test/java/restAssured/DataForTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.DataProvider;
 public class DataForTest {
     @DataProvider(name = "Data_for_Post")
     public Object[][] data_for_post(){
+
+        //providing the size of the matrix
 //        Object[][] data= new Object[3][2];
 //        data[0][0] = "Stephy";
 //        data[0][1] = 25;
@@ -15,10 +17,18 @@ public class DataForTest {
 //        data[2][0] = "Panda";
 //        data[2][1] = 25;
 
+        //without providing the size of the matrix
         return new Object[][]
                 {
                         {"bueb",4},
                         {"DjdbwGF",64}
                 };
+    }
+
+    @DataProvider(name = "Data_for_Delete")
+    public Object[] data_for_delete(){
+        return new Object[]{
+                "99bd","a9db"
+        };
     }
 }
